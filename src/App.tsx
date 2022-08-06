07fa896c9ea1ac2df3ec39a294eb6a660bada87f
Corbyn <corbyn.altman01@gmail.com>
2022-08-06 19:23:02 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./assets/scss/main.scss";
+import { FC } from "react";
+
+const App: FC = () => {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>;
+    </div>
+  );
+};
+
+export default App;
