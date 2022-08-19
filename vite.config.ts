3a7031edc702a5a89cd025eff001b871f723a2b2
Corbyn Altman <corbyn.altman01@gmail.com>
2022-08-18 21:57:06 -0400

    i hate service workers -_-

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import autoprefixer from "autoprefixer";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   css: {
     postcss: {
       plugins: [autoprefixer],
